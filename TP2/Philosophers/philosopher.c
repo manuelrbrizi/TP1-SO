a7d44d211fbdf4b0b225a014104f4d8b70a16d6f
Juan <juanmartinbaader@hotmail.com>
2019-05-31 17:46:18 -0300

    Philosophers terminado

--- TP2/Philosophers/philosopher.c
+++ TP2/Philosophers/philosopher.c
@@ -8,20 +8,25 @@
 enum status{Thinking,Hungry,Eating};
 
 void philosopher(int id){
+	enum status state;
 	while(1){
-		enum status state=Thinking;
+		state[id]=Thinking;
+		printCharAt("T",50+((id-1)/2)*50,200*((id%2)+1));
 		sleep(rand%3);
-		status=Hungry;
+		state[id]=Hungry;
+		printCharAt("H",50+((id-1)/2)*50,200*((id%2)+1));
 		if(id%2==0){
 			waitRight(id);
 			waitLeft(id);
-			status=Eating;
+			state[id]=Eating;
+			printCharAt("E",50+((id-1)/2)*50,200*((id%2)+1));
 			sleep(rand%3);
 		}
 		if(id%2==1){
 			waitLeft(id);
 			waitRight(id);
-			status=Eating;
+			state[id]=Eating;
+			printCharAt("E",50+((id-1)/2)*50,200*((id%2)+1));
 			sleep(rand%3);
 		}
 		putDownLeft(id);
@@ -47,12 +52,61 @@ void putDownRight(int id){
 }
 
 int main( int argc, char *argv[ ], char *envp[ ]){
-	int i=0;
-	while(i<3){
-		sem_create(i,1);
-		createPhilosopher(i);
+	int pids[10];
+	int totalP=1;
+	enum status state[20];
+	while(totalP<4){
+		sem_create(totalP*-1,1);
+		pids[totalP]=createPhilosopher(totalP);
 
-		i++;
+		totalP++;
 	}
-	
+	char command;
+	while(1){
+		command=get_char();
+		if(command==t)
+			return 1;
+		if(command=a){
+			if(totalP<=20){
+				sem_create(totalP);
+				pids[totalP]=createPhilosopher(totalP);
+				totalP++;
+			}
+			else {
+				print_string("only 20 philosophers at the same time");
+			}
+		}
+		if(command=d){
+			if(totalP<=2){
+				print_string("there needs to be at least 2 philosophers");
+			}
+			else{
+				totalP--;
+				deletePhilosopher(pids[totalP]);
+			}
+		}
+
+	}
+
 } 
+
+int createPhilosopher(int id){
+	Process_info_t* process;
+	process->p1=id;
+	process->p2;
+	process->p3;
+	process->ground='b';
+	process->tickets=100;
+	void (*ptr_funct)(int)=philosopher;
+	process-entryPoint=ptr_funct;
+	process->fd1=0;
+	process->fd2=1;
+	char buffy[10];
+	uintToBase(id,buffy,10);
+	process->process_name="philosopher "+buffy;
+	return execute(process);
+}
+
+void deletePhilosopher(int id){
+	kill(id);
+}
